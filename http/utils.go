0a9918f682942f250d7f03933e731cc056fa0291
David Subiros <subiros.david@gmail.com>
2020-08-13 15:09:48 +0200

    Copied DrainBody function from go-ns to dp-net/http

---
+++ http/utils.go
@@ -0,0 +1,27 @@
+package http
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/ONSdigital/log.go/log"
+)
+
+// DrainBody drains the body of the given of the given HTTP request.
+func DrainBody(r *http.Request) {
+
+	if r.Body == nil {
+		return
+	}
+
+	_, err := io.Copy(ioutil.Discard, r.Body)
+	if err != nil {
+		log.Event(r.Context(), "error draining request body", log.Error(err))
+	}
+
+	err = r.Body.Close()
+	if err != nil {
+		log.Event(r.Context(), "error closing request body", log.Error(err))
+	}
+}
