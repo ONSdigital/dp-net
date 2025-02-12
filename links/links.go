60ff8c14bae8c91a83e7b360e2f74621940ffe29
aryan-patel <aryanpatel1771@gmail.com>
2025-02-12 09:05:40 +0000

    rewriting functions for download links

--- links/links.go
+++ links/links.go
@@ -16,23 +16,18 @@ func FromHeadersOrDefault(h *http.Header, defaultURL *url.URL) *Builder {
 	path := h.Get("X-Forwarded-Path-Prefix")
 
 	host := h.Get("X-Forwarded-Host")
-	if host == "" || !strings.HasPrefix(host, "api.") {
+	if strings.HasPrefix(host, "api.") {
 		return &Builder{
-			URL: defaultURL.JoinPath(path),
+			URL: &url.URL{
+				Scheme: "https",
+				Host:   host,
+				Path:   path,
+			},
 		}
 	}
 
-	scheme := h.Get("X-Forwarded-Proto")
-	if scheme == "" {
-		scheme = "https"
-	}
-
 	return &Builder{
-		URL: &url.URL{
-			Scheme: scheme,
-			Host:   host,
-			Path:   path,
-		},
+		URL: defaultURL.JoinPath(path),
 	}
 }
 
@@ -54,4 +49,38 @@ func (b *Builder) BuildLink(link string) (string, error) {
 	}
 
 	return b.BuildURL(oldURL).String(), nil
+}
+
+func FromHeadersOrDefaultDownload(h *http.Header, defaultInternalURL, defaultExternalURL *url.URL) *Builder {
+	host := h.Get("X-Forwarded-Host")
+
+	if strings.HasPrefix(host, "api.") {
+		return &Builder{
+			URL: defaultExternalURL.JoinPath("/downloads"),
+		}
+	}
+
+	return &Builder{
+		URL: defaultInternalURL.JoinPath("/downloads"),
+	}
+}
+
+func (b *Builder) BuildDownloadURL(oldURL *url.URL) *url.URL {
+	newPath := oldURL.Path
+	for strings.HasPrefix(newPath, "/downloads") {
+		newPath = strings.TrimPrefix(newPath, "/downloads")
+	}
+
+	apiURL := b.URL.JoinPath(newPath)
+	apiURL.RawQuery = oldURL.RawQuery
+	return apiURL
+}
+
+func (b *Builder) BuildDownloadLink(link string) (string, error) {
+	oldURL, err := url.Parse(link)
+	if err != nil {
+		return "", errors.Wrap(err, "unable to parse link to URL")
+	}
+
+	return b.BuildDownloadURL(oldURL).String(), nil
 }
--- links/links_test.go
+++ links/links_test.go
@@ -8,122 +8,60 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_FromHeadersOrDefault(t *testing.T) {
+var (
+	defaultInternalURL         = url.URL{Scheme: "http", Host: "localhost:8080"}
+	defaultInternalDownloadURL = url.URL{Scheme: "http", Host: "localhost:23600"}
+	defaultExternalDownloadURL = url.URL{Scheme: "https", Host: "download.api.host"}
+)
 
+func Test_FromHeadersOrDefault(t *testing.T) {
 	Convey("Given a list of test cases", t, func() {
 		tests := []struct {
-			defaultURL    string
-			fwdProto      string
 			fwdHost       string
 			fwdPathPrefix string
 			want          string
 		}{
-			// Without any forwarded headers
+			// Without forwarded headers
 			{
-				"http://localhost:8080",
-				"",
 				"",
 				"",
 				"http://localhost:8080",
 			},
-			// With all forwarded headers
+			// With forwarded host and path prefix
 			{
-				"http://localhost:8080",
-				"https",
 				"api.external.host",
 				"prefix",
 				"https://api.external.host/prefix",
 			},
-			// With only forwarded proto
+			// With forwarded host
 			{
-				"http://localhost:8080",
-				"https",
-				"",
-				"",
-				"http://localhost:8080",
-			},
-			// With only forwarded host
-			{
-				"http://localhost:8080",
-				"",
 				"api.external.host",
 				"",
 				"https://api.external.host",
 			},
-			// With only forwarded path prefix
+			// With forwarded path prefix
 			{
-				"http://localhost:8080",
-				"",
 				"",
 				"prefix",
 				"http://localhost:8080/prefix",
 			},
-			// Without all headers except forwarded proto
+			// With internal forwarded host
 			{
-				"http://localhost:8080",
-				"",
-				"api.external.host",
-				"prefix",
-				"https://api.external.host/prefix",
-			},
-			// Without all headers except forwarded host
-			{
-				"http://localhost:8080",
-				"https",
-				"",
-				"prefix",
-				"http://localhost:8080/prefix",
-			},
-			// Without all headers except forwarded path prefix
-			{
-				"http://localhost:8080",
-				"https",
-				"api.external.host",
-				"",
-				"https://api.external.host",
-			},
-			// With only forwarded proto and host
-			{
-				"http://localhost:8080",
-				"https",
-				"api.external.host",
-				"",
-				"https://api.external.host",
-			},
-			// With only forwarded prefix and host
-			{
-				"http://localhost:8080",
-				"",
-				"api.external.host",
-				"prefix",
-				"https://api.external.host/prefix",
-			},
-			// With only forwarded proto and prefix
-			{
-				"http://localhost:8080",
-				"https",
-				"",
-				"prefix",
-				"http://localhost:8080/prefix",
-			},
-			// With non-external forwarded host
-			{
-				"http://localhost:8080",
-				"",
 				"internalhost",
 				"",
 				"http://localhost:8080",
 			},
+			// With internal forwarded host and path prefix
+			{
+				"internalhost",
+				"prefix",
+				"http://localhost:8080/prefix",
+			},
 		}
 
 		for _, tt := range tests {
-			du, err := url.Parse(tt.defaultURL)
-			So(err, ShouldBeNil)
-
 			h := http.Header{}
-			if tt.fwdProto != "" {
-				h.Add("X-Forwarded-Proto", tt.fwdProto)
-			}
+
 			if tt.fwdHost != "" {
 				h.Add("X-Forwarded-Host", tt.fwdHost)
 			}
@@ -131,20 +69,14 @@ func Test_FromHeadersOrDefault(t *testing.T) {
 				h.Add("X-Forwarded-Path-Prefix", tt.fwdPathPrefix)
 			}
 
-			du.JoinPath()
-
-			builder := FromHeadersOrDefault(&h, du)
+			builder := FromHeadersOrDefault(&h, &defaultInternalURL)
 			So(builder, ShouldNotBeNil)
-			So(builder.URL, ShouldNotBeNil)
 			So(builder.URL.String(), ShouldEqual, tt.want)
-
 		}
-
 	})
 }
 
-func TestBuilder_BuildLink(t *testing.T) {
-
+func Test_BuildLink(t *testing.T) {
 	Convey("Given a list of test cases", t, func() {
 		tests := []struct {
 			builderURL string
@@ -226,7 +158,6 @@ func TestBuilder_BuildLink(t *testing.T) {
 		}
 
 		for _, tt := range tests {
-
 			bu, err := url.Parse(tt.builderURL)
 			So(err, ShouldBeNil)
 			builder := &Builder{URL: bu}
@@ -238,7 +169,6 @@ func TestBuilder_BuildLink(t *testing.T) {
 			// Check that the function hasn't modified the builder's internal URL
 			So(builder.URL.String(), ShouldEqual, tt.builderURL)
 		}
-
 	})
 
 	Convey("When an invalid old URL is provided", t, func() {
@@ -249,26 +179,146 @@ func TestBuilder_BuildLink(t *testing.T) {
 		So(err.Error(), ShouldContainSubstring, "unable to parse link to URL")
 		So(newurl, ShouldBeEmpty)
 	})
-
 }
 
-func Test_FromHeadersOrDefault_NonApiHost(t *testing.T) {
-	Convey("Given a non-api forwarded host", t, func() {
-		defaultURL, err := url.Parse("http://localhost:8080")
-		So(err, ShouldBeNil)
+func Test_FromHeadersOrDefaultDownload(t *testing.T) {
+	Convey("Given a list of test cases", t, func() {
+		tests := []struct {
+			fwdHost string
+			want    string
+		}{
+			// Without a forwarded host
+			{
+				"",
+				"http://localhost:23600/downloads",
+			},
+			// With an external forwarded host
+			{
+				"api.external.host",
+				"https://download.api.host/downloads",
+			},
+			// With an internal forwarded host
+			{
+				"internalhost",
+				"http://localhost:23600/downloads",
+			},
+		}
 
-		h := http.Header{}
-		h.Add("X-Forwarded-Host", "internalhost")
-		h.Add("X-Forwarded-Path-Prefix", "prefix")
+		for _, tt := range tests {
+			h := http.Header{}
 
-		Convey("When the builder is created", func() {
-			builder := FromHeadersOrDefault(&h, defaultURL)
+			if tt.fwdHost != "" {
+				h.Add("X-Forwarded-Host", tt.fwdHost)
+			}
 
-			Convey("Then the builder URL should be the default URL with the path prefix", func() {
-				So(builder, ShouldNotBeNil)
-				So(builder.URL, ShouldNotBeNil)
-				So(builder.URL.String(), ShouldEqual, "http://localhost:8080/prefix")
-			})
-		})
+			builder := FromHeadersOrDefaultDownload(&h, &defaultInternalDownloadURL, &defaultExternalDownloadURL)
+			So(builder, ShouldNotBeNil)
+			So(builder.URL.String(), ShouldEqual, tt.want)
+		}
+	})
+}
+
+func Test_BuildDownloadLink(t *testing.T) {
+	Convey("Given a list of test cases", t, func() {
+		tests := []struct {
+			builderURL string
+			oldLink    string
+			want       string
+		}{
+			// Empty old link
+			{
+				"http://localhost:23600/downloads",
+				"",
+				"http://localhost:23600/downloads",
+			},
+			// Old link with no path
+			{
+				"http://localhost:23600/downloads",
+				"http://localhost:23600",
+				"http://localhost:23600/downloads",
+			},
+			// Old link with different base url
+			{
+				"http://localhost:23600/downloads",
+				"https://oldHost:1000",
+				"http://localhost:23600/downloads",
+			},
+			// Old link with path
+			{
+				"http://localhost:23600/downloads",
+				"http://localhost:23600/some/path",
+				"http://localhost:23600/downloads/some/path",
+			},
+			// Old link with path and different base url
+			{
+				"http://localhost:23600/downloads",
+				"http://oldHost:1000/some/path",
+				"http://localhost:23600/downloads/some/path",
+			},
+			// Old link without base url
+			{
+				"http://localhost:23600/downloads",
+				"/some/path",
+				"http://localhost:23600/downloads/some/path",
+			},
+			// Old link with query params
+			{
+				"http://localhost:23600/downloads",
+				"http://localhost:23600/some/path?param1=value1&param2=value2",
+				"http://localhost:23600/downloads/some/path?param1=value1&param2=value2",
+			},
+			// Old external link to new internal url
+			{
+				"http://localhost:23600/downloads",
+				"https://download.api.host/downloads/some/path",
+				"http://localhost:23600/downloads/some/path",
+			},
+			// Old external link to new external url
+			{
+				"https://download.api.host/downloads",
+				"https://download.api.host/downloads/some/path",
+				"https://download.api.host/downloads/some/path",
+			},
+			// Old external link to new external url (multiple /downloads)
+			{
+				"https://download.api.host/downloads",
+				"https://download.api.host/downloads/downloads/downloads/some/path",
+				"https://download.api.host/downloads/some/path",
+			},
+			// Old internal link to new external url
+			{
+				"https://download.api.host/downloads",
+				"http://localhost:23600/downloads/some/path",
+				"https://download.api.host/downloads/some/path",
+			},
+			// Old internal link to new external url with query params
+			{
+				"https://download.api.host/downloads",
+				"http://localhost:23600/downloads/some/path?param1=value1&param2=value2",
+				"https://download.api.host/downloads/some/path?param1=value1&param2=value2",
+			},
+		}
+
+		for _, tt := range tests {
+			bu, err := url.Parse(tt.builderURL)
+			So(err, ShouldBeNil)
+			builder := &Builder{URL: bu}
+
+			newurl, err := builder.BuildDownloadLink(tt.oldLink)
+			So(err, ShouldBeNil)
+			So(newurl, ShouldEqual, tt.want)
+
+			// Check that the function hasn't modified the builder's internal URL
+			So(builder.URL.String(), ShouldEqual, tt.builderURL)
+		}
+	})
+
+	Convey("When an invalid old URL is provided", t, func() {
+		builder := &Builder{URL: &url.URL{}}
+		invalidURL := ":invalid/url"
+		newurl, err := builder.BuildDownloadLink(invalidURL)
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldContainSubstring, "unable to parse link to URL")
+		So(newurl, ShouldBeEmpty)
 	})
 }
